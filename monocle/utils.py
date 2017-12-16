ee0fd66b2493430fef5c40936595bd7206b089a0
Chrales <chrales@gmail.com>
2017-12-16 23:45:28 +0100

    remove iOS 8

--- monocle/utils.py
+++ monocle/utils.py
@@ -135,7 +135,6 @@ def get_device_info(account):
 
 
 def generate_device_info(account):
-    ios8 = ('8.0', '8.0.1', '8.0.2', '8.1', '8.1.1', '8.1.2', '8.1.3', '8.2', '8.3', '8.4', '8.4.1')
     ios9 = ('9.0', '9.0.1', '9.0.2', '9.1', '9.2', '9.2.1', '9.3', '9.3.1', '9.3.2', '9.3.3', '9.3.4', '9.3.5')
     # 10.0 was only for iPhone 7 and 7 Plus, and is rare
     ios10 = ('10.0.1', '10.0.2', '10.0.3', '10.1', '10.1.1', '10.2', '10.2.1', '10.3', '10.3.1', '10.3.2', '10.3.3')
@@ -154,7 +153,7 @@ def generate_device_info(account):
         # iPhone SE started on 9.3
         account['iOS'] = choice(('9.3', '9.3.1', '9.3.2', '9.3.3', '9.3.4', '9.3.5') + ios10)
     else:
-        account['iOS'] = choice(ios8 + ios9 + ios10)
+        account['iOS'] = choice(ios9 + ios10)
 
     return account
 
